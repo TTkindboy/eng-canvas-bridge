f63e458f0a507ea2827c8b9daba5c5ea04210cf9
Tanner Klipstein <64111041+TTkindboy@users.noreply.github.com>
2026-03-22 18:40:54 +0000

    Update formatting to be PEP 8

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -6,8 +6,10 @@ from fastapi import Depends, Request
 from pydantic import computed_field
 from pydantic_settings import BaseSettings
 
+
 async def get_client(request: Request) -> httpx.AsyncClient:
     return request.state.http_client
+
 
 type HTTPClient = Annotated[httpx.AsyncClient, Depends(get_client)]
 
@@ -21,14 +23,16 @@ class Settings(BaseSettings):
     def api_url(self) -> str:
         return f"{self.site_url}/api/v1"
 
+
 @cache
 def get_settings() -> Settings:
     """Return the cached application settings instance."""
     return Settings()  # ty:ignore[missing-argument]
 
+
 type SettingsDep = Annotated[Settings, Depends(get_settings)]
 
 
-@cache # maybe remove cache in future bc get_settings() is already cached
-def canvas_auth(): # temporary until OAuth2
+@cache  # maybe remove cache in future bc get_settings() is already cached
+def canvas_auth():  # temporary until OAuth2
     return {"Authorization": f"Bearer {get_settings().canvas_api_key}"}
