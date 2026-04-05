af3424af6f2722eb241313ce9e4419d101dbb6e2
TTkindboi <taklipstein@icloud.com>
2026-04-05 03:34:39 +0200

    Revert "Re-introduce bug for CI testing"
    This reverts commit d2bf136c9da06449856d83dff1b485bd898cf824.

--- backend/app/pdf_parsing.py
+++ backend/app/pdf_parsing.py
@@ -18,7 +18,7 @@ WEEKDAY_MAP = { # TODO: ADD WEEKEND PARSING
     "M": calendar.MONDAY,
     "T": calendar.TUESDAY,
     "W": calendar.WEDNESDAY,
-    "OP81": calendar.THURSDAY,
+    "Th": calendar.THURSDAY,
     "TH": calendar.THURSDAY, # Capital
     "F": calendar.FRIDAY,
 }
