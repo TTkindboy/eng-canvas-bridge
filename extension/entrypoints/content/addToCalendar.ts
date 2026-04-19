6a006f75b15746b8e9a230b100359134bc91434d
TTkindboi <taklipstein@icloud.com>
2026-04-19 17:58:21 -0400

    Add backend calls

--- extension/entrypoints/content/addToCalendar.ts
+++ extension/entrypoints/content/addToCalendar.ts
@@ -1,3 +1,4 @@
+import { previewUploadedSchedule } from '@/lib/client/sdk.gen';
 import { client } from '../../lib/client/client.gen';
 
 client.setConfig({
@@ -5,6 +6,32 @@ client.setConfig({
   credentials: 'include',
 });
 
+
+function getCanvasPdfDownloadUrl(fileId: string): string {
+  return `${window.location.origin}/files/${fileId}/download`
+}
+
+function getCanvasFileId(url = window.location.href): string | null {
+  return new URL(url).pathname.match(/\/files\/(\d+)/)?.[1] ?? null
+}
+
 export async function handleAddToCalendar() {
-  alert('Button clicked!');
+  const pdfResponse = await fetch(getCanvasPdfDownloadUrl(getCanvasFileId() ?? ''), {
+    credentials: 'include',
+  })
+
+  if (!pdfResponse.ok) {
+    throw new Error(`Failed to download Canvas file ${getCanvasFileId()}: ${pdfResponse.status}`)
+  }
+
+  const pdfBlob = await pdfResponse.blob()
+
+  const { data: schedule } = await previewUploadedSchedule({
+    body: { pdf: pdfBlob },
+    throwOnError: true,
+  })
+
+  console.log('Parsed schedule:', schedule)
+
+  alert(`Button clicked!\nFile ID: ${getCanvasFileId()}`)
 }
